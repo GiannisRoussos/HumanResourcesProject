b1796b754fca039f4ed2c30f1d084a2fc11379fb
GiannisRoussos <john71980@hotmail.com>
2024-09-10 23:47:21 +0300

    HumanResources

---
+++ src/test/java/mycompany/humanresources/HumanResourcesApplicationTests.java
@@ -0,0 +1,13 @@
+package mycompany.humanresources;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanResourcesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
